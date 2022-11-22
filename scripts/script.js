22d20b7c07ca7708410fc99e969817f8884d523f
a-rede <abell.rede1@outlook.com>
2022-11-22 10:23:06 -0800

    remove even listener

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,35 @@
 //* Example 1 - JS and DOM Events
 
-const clickMeButton = document.querySelector(".click-me");
+// const clickMeButton = document.querySelector(".click-me");
 
-clickMeButton.addEventListener("click", () => {
-    alert('Something');
-});
+// clickMeButton.addEventListener("click", () => {
+//     alert('Something');
+// });
 
 // clickMeButton.addEventListener("click", handClickMeButton);
 
 // function handClickMeButton() {
 //     alert("something");
 // }; 
+
+//* Example 2 - Remove Event Handler
+
+// Moving removeEventListener inside the function
+// const clickMeButton = document.querySelector(".click-me");
+
+// function handClickMeButton() {
+//     alert("something");
+//     clickMeButton.removeEventListener("click", handClickMeButton);
+// }; 
+
+// clickMeButton.addEventListener("click", handClickMeButton);
+
+// Using 
+const clickMeButton = document.querySelector(".click-me");
+
+function handClickMeButton() {
+    alert("something");
+}; 
+
+clickMeButton.addEventListener("click", handClickMeButton, {once:true});
+//* Example 3 - Multiple Event Handlers
